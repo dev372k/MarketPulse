6a1c38889e477a3cdcf56fd7de2c053476feb3cb
S.M.Owais <smowais.neduet@gmail.com>
2024-05-23 13:40:09 +0500

    add loader

--- MT/Presentation/wwwroot/js/site.js
+++ MT/Presentation/wwwroot/js/site.js
@@ -36,3 +36,21 @@ async function request(url, method, data = null) {
         throw new Error(error.message || 'Something went wrong');
     }
 }
+
+$.LoadingOverlaySetup({
+    background: "rgba(0, 0, 0, 0.5)",
+    image: '<svg width="120" height="30" xmlns="http://www.w3.org/2000/svg" fill="#000" viewBox="0 0 120 30"><circle cx="15" cy="15" r="15"><animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite"/><animate attributeName="fill-opacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1" calcMode="linear" repeatCount="indefinite"/></circle><circle cx="60" cy="15" r="9" attributeName="fill-opacity" from="1" to="0.3"><animate attributeName="r" from="9" to="9" begin="0s" dur="0.8s" values="9;15;9" calcMode="linear" repeatCount="indefinite"/><animate attributeName="fill-opacity" from="0.5" to="1" begin="0s" dur="0.8s" values=".5;1;.5" calcMode="linear" repeatCount="indefinite"/></circle><circle cx="105" cy="15" r="15"><animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite"/><animate attributeName="fill-opacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1" calcMode="linear" repeatCount="indefinite"/></circle></svg>',
+    imageAnimation: "1.5s fadein",
+    imageColor: "#800020"
+});
+
+//$(document).ajaxStart(function () {
+//    $.LoadingOverlay("show");
+//});
+//$(document).ajaxStop(function () {
+//    $.LoadingOverlay("hide");
+//});
+
+const loading = (type) => {
+    $.LoadingOverlay(type);
+}
